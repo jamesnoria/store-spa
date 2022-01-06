25c947b47069e2b14b12e7983019be3da71bd902
James Noria <jamesnoria@pm.me>
2022-01-06 12:59:55 -0500

    404 page added

--- src/templates/Header.js
+++ src/templates/Header.js
@@ -147,21 +147,21 @@ const Header = () => {
 					</li>
 					<li>
 						<a
-							href="#"
+							href="#/Services"
 							class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
 							>Services</a
 						>
 					</li>
 					<li>
 						<a
-							href="#"
+							href="#/Pricing"
 							class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
 							>Pricing</a
 						>
 					</li>
 					<li>
 						<a
-							href="#"
+							href="#/Contact"
 							class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
 							>Contact</a
 						>
